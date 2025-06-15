37891b77a964ac1dc32f2b08977886b8a411e69c
Jonathan Boahene <jonathanboahene@gmail.com>
2025-06-15 23:13:12 +0100

    added new files

--- index.js
+++ index.js
@@ -15,8 +15,25 @@ function test() {
 
 var text = prompt("Write your text here");
 var textCount = text.length;
-alert("You have written " + textCount + " characters, you have have " + (140 - textCount) + " characters remaining");
+alert(
+  "You have written " +
+    textCount +
+    " characters, you have have " +
+    (140 - textCount) +
+    " characters remaining"
+);
 
-var message = prompt("What's your name?");
-var realName = message.toUpperCase();
+//Question: create a program that asks a user their firstname and greets them with a capatilise firstname
+//Create a var that stores user's name via propmt
+
+//Capitalise the first letter, to do this we need slice and toUpperCase
+
+//Create a var for the rest of username
+
+//Concatenate first char with the rest of the chars
+
+var username = prompt("What's your firstname?");
+var firstChar = username.slice(0, 1).toUpperCase();
+var restOfUserName = username.slice(1, -1); //could use the .length function to solve
+var realName = firstChar + restOfUserName;
 console.log("Hello " + realName);
