834f634780ed6636cd0197abfdb581a0c14a5410
Abayie <jonathanboahene@gmail.com>
2025-06-30 23:43:57 +0100

    if else

--- index.js
+++ index.js
@@ -73,3 +73,17 @@ function lifeInWeeks(num) {
 let lovePercentage = Math.random() * 100;
 lovePercentage = Math.floor(lovePercentage) + 1; 
 console.log(lovePercentage);
+
+
+function bmiCalculator(weight, height) {
+  let bmi = weight / Math.pow(height, 2);
+  if (bmi < 18.5) {
+    return "Your BMI is " + bmi + ", so you are underweight.";
+  }
+  if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Your BMI is " + bmi + ", so you have a normal weight.";
+  }
+  if (bmi > 24.9) {
+    return "Your BMI is " + bmi + ", so you are overweight.";
+  }
+}
